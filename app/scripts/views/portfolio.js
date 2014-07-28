ecdae86959773c7b4deacaba79ec08c3e950bc00
Chris Mendes <chrismendes@Chriss-MacBook-Air.local>
2014-07-28 08:51:07 +0100

    Added mouse over/exit animation on portfolio boxes

--- app/scripts/views/portfolio.js
+++ app/scripts/views/portfolio.js
@@ -16,29 +16,44 @@ define([
         background:     'gold',
         theme:          'red',
 
-        // events: {
-        //     'click .back-home': 'returnToHomepage'
-        // },
+        events: {
+            'mouseenter .grid-content-block':      'onBoxHover',
+            'mouseleave .grid-content-block':      'onBoxHoverOut',
+        },
+
+        // Box hover over/out effects
+        onBoxHover: function(e) {
+            console.log('onBoxHover');
+            this.identifyBox(e).not('.is-filtered-out').addClass('is-active', 200);
+        },
+        onBoxHoverOut: function(e) {
+            this.identifyBox(e).removeClass('is-active', 200);
+        },
 
         onAfterRender: function() {
-            this.setCarouselAnnotationHook();
+            // this.setCarouselAnnotationHook();
         },
 
-        setCarouselAnnotationHook: function() {
-            this.setCarouselAnnotation();
-            var self = this;
-            $('#carousel-allprojects').on('slid.bs.carousel', function() {
-                self.setCarouselAnnotation();
-                $('.carousel-annotation').fadeIn(150);
-            });
-            $('#carousel-allprojects').on('slide.bs.carousel', function() {
-                $('.carousel-annotation').fadeOut(500);
-            });
-        },
-        setCarouselAnnotation: function() {
-            var annotation = $('#carousel-allprojects').find('.item.active .annotation').html();
-            $('.carousel-annotation').html(annotation);
+        // Return jQuery element for clicked box, whether box itself or child element clicked
+        identifyBox: function(e) {
+            return (e.target.tagName != 'DIV') ? $(e.target).parent() : $(e.target);
         }
+
+        // setCarouselAnnotationHook: function() {
+        //     this.setCarouselAnnotation();
+        //     var self = this;
+        //     $('#carousel-allprojects').on('slid.bs.carousel', function() {
+        //         self.setCarouselAnnotation();
+        //         $('.carousel-annotation').fadeIn(150);
+        //     });
+        //     $('#carousel-allprojects').on('slide.bs.carousel', function() {
+        //         $('.carousel-annotation').fadeOut(500);
+        //     });
+        // },
+        // setCarouselAnnotation: function() {
+        //     var annotation = $('#carousel-allprojects').find('.item.active .annotation').html();
+        //     $('.carousel-annotation').html(annotation);
+        // }
 
     });
 
