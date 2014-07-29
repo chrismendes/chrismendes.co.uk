755a09838ae3d685266c7417864fa125102a3d9e
Chris Mendes <inbox@chrismendes.co.uk>
2014-07-29 23:52:51 +0100

    Current situation page basic version done

--- app/scripts/views/situation.js
+++ app/scripts/views/situation.js
@@ -2,27 +2,17 @@
 // Current Situation Page
 // -------
 define([
-    'jquery',
-    'backbone',
     'views/_base',
     'text!/templates/situation.html',
     'common'
-], function($, Backbone, BaseView, html, Common) {
+], function(BaseView, html, Common) {
 
     var CurrentSituationView = BaseView.extend({
 
         template:       _.template(html),
         elPage:         '#page-situation',
-        background:     'denim',
-        theme:          'green',
-
-        events: {
-            
-        },
-
-        onAfterRender: function() {
-            // this.setCarouselAnnotationHook();
-        },
+        background:     'denimblue',
+        theme:          'green'
 
     });
 
