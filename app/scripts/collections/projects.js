98c23d7d107e4abfa10cd822e239f14c85242d73
Chris Mendes <inbox@chrismendes.co.uk>
2014-08-06 08:32:59 +0100

    Added project model/collection

---
+++ app/scripts/collections/projects.js
@@ -0,0 +1,19 @@
+// -------
+// Projects Collection
+// -------
+define([
+    'backbone',
+    'models/project'
+], function(Backbone, ProjectModel) {
+
+    var ProjectsCollection = Backbone.Collection.extend({
+
+        model: ProjectModel,
+
+        url: function() {
+            return '';
+        }
+
+    });
+
+}
