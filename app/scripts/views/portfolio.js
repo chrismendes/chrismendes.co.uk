68ad125f5c53516c1ab7c6d9887c4822935f572f
Chris Mendes <chrismendes@Chriss-MacBook-Air.local>
2014-08-13 08:50:55 +0100

    Implemented basic portfolio template

--- app/scripts/views/portfolio.js
+++ app/scripts/views/portfolio.js
@@ -23,6 +23,42 @@ define([
 
         initialize: function() {
             var projects = new ProjectCollection(JSON.parse(PortfolioData));
+            this.prepareData(projects);
+        },
+
+        prepareData: function(collection) {
+            // TODO: Parse to JSON rather than passing down models to view
+            var projects = {};
+            
+            projects.highlights = collection.filter(function(item) {
+                return item.get('highlight') === true;
+            });
+
+            projects.contract = collection.filter(function(item) {
+                return item.get('contract') === true;
+            });
+
+            projects.dnx = collection.filter(function(item) {
+                return item.get('employer') === 'DNX Marketing';
+            });
+
+            projects.fingo = collection.filter(function(item) {
+                return item.get('employer') === 'Fingo Marketing';
+            });
+
+            projects.other = collection.filter(function(item) {
+                return item.get('contract') === false && item.get('employer') === null;
+            });
+
+            this.data = {
+                categories: {
+                    'highlights':   { name: 'Project Highlights',   projects: projects.highlights },
+                    'contract':     { name: 'Contract Work',        projects: projects.contract },
+                    'dnx':          { name: 'DNX Marketing',        projects: projects.dnx },
+                    'fingo':        { name: 'Fingo Marketing',      projects: projects.fingo },
+                    'other':        { name: 'Other Projects',       projects: projects.other }
+                }
+            };
         },
 
         onAfterRender: function() {
