ebcc7b0c3e140e07dee4fd3bec0c5e23a8e8f8ad
Chris Mendes <inbox@chrismendes.co.uk>
2014-09-18 09:13:01 +0100

    Added tab functionality to workflow page

--- app/scripts/views/workflow.js
+++ app/scripts/views/workflow.js
@@ -2,17 +2,37 @@
 // Dev Workflow Page
 // -------
 define([
+    'jqueryui',
     'views/_base',
     'text!/templates/workflow.html',
     'common'
-], function(BaseView, html, Common) {
+], function($, BaseView, html, Common) {
 
     var WorkflowView = BaseView.extend({
 
         template:       _.template(html),
         elPage:         '#page-workflow',
         background:     'teal',
-        theme:          'green'
+        theme:          'green',
+
+        events: {
+            'click .js-select':    'onBoxClick'
+        },
+
+        onBoxClick: function(e) {
+            var selected = this.identifyClickedBox(e);
+            this.setActive(selected);
+            this.changeContent(selected.attr('data-tab'));
+        },
+
+        setActive: function(selected) {
+            $('.js-select').removeClass('is-active');
+            selected.toggleClass('is-active');
+        },
+        changeContent: function(tab) {
+            $('.js-tab').hide();
+            $('#'+tab).show();
+        }
 
     });
 
