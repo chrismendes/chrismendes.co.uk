1185c76320490f525c61e393ac9029f490f15f1b
Chris Mendes <inbox@chrismendes.co.uk>
2014-10-17 20:51:27 +0100

    requirejs/main/app level tidyup

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,68 +1,13 @@
 // -------
-// RequireJS Config
-// -------
-require.config({
-    baseURL: 'app/scripts',
-    paths: {
-        'text':                     '../libraries/requirejs-text/text',
-        'jquery':                   '../libraries/jquery/dist/jquery',
-        'jqueryui':                 '../libraries/jquery-ui/ui/jquery-ui',
-        'underscore':               '../libraries/underscore/underscore',
-        'backbone':                 '../libraries/backbone/backbone',
-        'bootstrap-modal':          '../libraries/bootstrap/js/modal',
-        'bootstrap-transition':     '../libraries/bootstrap/js/transition',
-        'bootstrap-tab':            '../libraries/bootstrap/js/tab',
-        'bootstrap-collapse':       '../libraries/bootstrap/js/collapse',
-        'bootstrap-carousel':       '../libraries/bootstrap/js/carousel',
-        'owlcarousel':              '../libraries/owlcarousel/owl-carousel/owl.carousel',
-        'fastclick':                '../libraries/fastclick-amd/fastclick',
-    },
-    shim: {
-        'underscore': {
-            exports: '_'
-        },
-        'backbone': {
-            exports: 'Backbone'
-        },
-        'jqueryui': {
-            exports: '$',
-            deps:    ['jquery']
-        },
-        'bootstrap-carousel': {
-            exports: 'bootstrap-carousel',
-            deps:    ['jquery']
-        },
-        'bootstrap-modal': {
-            exports: 'bootstrap-modal',
-            deps:    ['jquery']
-        },
-        'bootstrap-transition': {
-            exports: 'bootstrap-transition',
-            deps:    ['jquery']
-        },
-        'bootstrap-tab': {
-            exports: 'bootstrap-tab',
-            deps:    ['jquery']
-        },
-        'bootstrap-collapse': {
-            exports: 'bootstrap-collapse',
-            deps:    ['jquery']
-        },
-        'owlcarousel': {
-            exports: 'owlcarousel',
-            deps:    ['jquery']
-        }
-    }
-});
-
-
-// -------
 // App Start
 // -------
-require([
-    'routers/router'
-], function(Router) {
+define(['require', '../config'], function (require) {
+
     'use strict';
-    /* jshint nonew: false */
-    new Router();
+
+    require(['routers/router'], function(AppRouter) {
+        /* jshint nonew: false */
+        new AppRouter();
+    });
+
 });
