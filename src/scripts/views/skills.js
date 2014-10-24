70d845b429b0709db6b59025ae8cf11f06d9124f
Chris Mendes <inbox@chrismendes.co.uk>
2014-10-24 15:06:11 +0100

    Removed redundant code (page fadein, now in base)

--- src/scripts/views/skills.js
+++ src/scripts/views/skills.js
@@ -1,5 +1,5 @@
 // -------
-// CV Page
+// Skills/Expertise Page
 // -------
 define([
     'jquery',
@@ -17,49 +17,7 @@ define([
         id:             'skills',
         template:       _.template(html),
         background:     'gold',
-        theme:          'red',
-
-        events: {
-            'click .js-nav':        'changeTab'
-        },
-
-        // Fade out current body content, fade in new
-        changeTab: function(e) {
-            var boxClicked = this.identifyBox(e);
-            var reqContent = boxClicked.attr('data-body');
-
-            if(boxClicked.hasClass('is-active')) {
-                return;
-            }
-
-            $('.js-nav-'+reqContent+' .js-tab-link').tab('show');
-            
-            // Change sidenav states
-            // $('.js-nav.is-active').removeClass('is-active', 100);
-            // boxClicked.addClass('is-active', 100);
-
-            // Change body content
-            // $('.js-body.is-active').fadeOut(200, function() {
-            //     $(this).removeClass('is-active');
-            //     $('.js-body-'+reqContent).fadeIn(200, function() {
-            //         $(this).addClass('is-active');
-            //     });
-            // });
-        },
-
-        // Return jQuery element for clicked box, whether box itself or child element clicked
-        identifyBox: function(e) {
-            return (e.target.tagName !== 'DIV') ? $(e.target).parents('.js-nav') : $(e.target);
-        },
-
-        // Page entrance animations
-        onAfterRender: function() {
-            // Slide in category column
-            $('.js-animated-entrace').removeClass('is-off-stage', 300);
-
-            // Fade in main body
-            $('.js-animated-entrace').removeClass('is-off-stage', 300);
-        }
+        theme:          'red'
 
     });
 
