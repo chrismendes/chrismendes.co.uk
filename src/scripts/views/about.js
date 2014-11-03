3650b739d57846c7f8c362998482681ddf3ac1fd
Chris Mendes <inbox@chrismendes.co.uk>
2014-11-03 22:53:13 +0000

    Added basic carousel functionality/presentation to about me page

--- src/scripts/views/about.js
+++ src/scripts/views/about.js
@@ -2,10 +2,12 @@
 // About Me Page
 // -------
 define([
+    'jquery',
     'underscore',
     'views/_base',
-    'text!../../templates/about.html'
-], function(_, BaseView, html) {
+    'text!../../templates/about.html',
+    'owlcarousel'
+], function($, _, BaseView, html) {
 
     'use strict';
 
@@ -14,7 +16,40 @@ define([
         id:             'about',
         template:       _.template(html),
         background:     'teal',
-        theme:          'green'
+        theme:          'red',
+
+        onBeforeShow: function() {
+            this.startCarousels();
+        },
+
+        startCarousels: function() {
+            $('.js-carousel').owlCarousel({
+                singleItem: true,
+                autoWidth: true,
+                pagination: false
+            });
+            this.setCarouselNavClickEvents();
+        },
+
+        setCarouselNavClickEvents: function() {
+            var owl = $('.js-carousel').data('owlCarousel');
+            $('.js-slide-link').click(function() {
+                var s = $(this).attr('data-slide');
+                if(typeof s !== 'undefined') {
+                    owl.goTo(s);
+                    $('.js-slide-link').removeClass('is-active');
+                    $(this).addClass('is-active');
+                }
+            });
+            $('.js-carousel-previous').click(function(e) {
+                e.preventDefault();
+                owl.prev();
+            });
+            $('.js-carousel-next').click(function(e) {
+                e.preventDefault();
+                owl.next();
+            });
+        },
 
     });
 
