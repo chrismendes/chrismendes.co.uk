d538d0ccf257ac19c186a1e0c60fd14537abc1f9
Chris Mendes <inbox@chrismendes.co.uk>
2015-08-01 13:18:57 +0100

    Work in progress of gulp copy images given new image file org

--- gulp/tasks/copy.js
+++ gulp/tasks/copy.js
@@ -3,6 +3,7 @@ var baseURL      = path.resolve(__dirname, '../../');
 
 var config       = require(baseURL + '/app.config.js');
 var gulp         = require('gulp');
+var flatten      = require('gulp-flatten');
 
 
 gulp.task('copy:fonts', function() {
@@ -12,10 +13,21 @@ gulp.task('copy:fonts', function() {
 
 });
 
-gulp.task('copy:images', function() {
+
+gulp.task('copy:images', ['images:global', 'images:non-global']);
+
+
+gulp.task('images:global', function() {
+
+  gulp.src(config.directories.src.images + '/**/*')
+    .pipe(gulp.dest(config.directories.build.images));
+
+});
+
+
+gulp.task('images:non-global', function() {
 
   var targetDirs = [
-    config.directories.src.images     + '/**/*',
     config.directories.src.components + '/**/images/**',
     config.directories.src.pages      + '/**/images/**'
   ];
