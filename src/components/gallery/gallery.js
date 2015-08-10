bdb5e7cba41f2e83e0adecd4525f634a3abf0be6
Chris Mendes <inbox@chrismendes.co.uk>
2015-08-10 08:33:37 +0100

    Better arranged gallery pagination controls. Added pagination functionality

--- src/components/gallery/gallery.js
+++ src/components/gallery/gallery.js
@@ -9,15 +9,33 @@ require('jcarousellite/jcarousellite');
 // ---
 // Component
 // ---
+var selectors = {
+  pagination: '.js-pagination > ul > li'
+};
+
 var component = {
 
   initialise: function($target) {
+    var pagination = component.getPaginationButtons();
+
     $target.jCarouselLite({
       btnNext:  '.js-gallery-next',
       btnPrev:  '.js-gallery-previous',
+      btnGo:    pagination,
       visible:  1,
       circular: false
     });
+  },
+
+  getPaginationButtons: function() {
+    var buttons = [];
+    var count = $(selectors.pagination).length;
+    for(var i = 0; i < count; i++) {
+      var suffix = ':nth-child(' + (i+1) + ')';
+      buttons.push(selectors.pagination + suffix);
+    }
+
+    return buttons;
   }
 
 };
