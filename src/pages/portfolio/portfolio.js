0360fa6c4aa0735b6edad8140a4eeb8569c17c3f
Chris Mendes <inbox@chrismendes.co.uk>
2015-08-10 21:05:19 +0100

    Added project screenshot data to markup for offcanvas panel population purposes

--- src/pages/portfolio/portfolio.js
+++ src/pages/portfolio/portfolio.js
@@ -12,7 +12,7 @@ $(document).on('offCanvasPanelOpen', function(event, $data) {
   var $target = $('.js-offcanvaspanel-loadedcontent');
 
   var projectName = $data.attr('data-name');
-  var projectURL = $data.attr('data-url');
+  var projectURL  = $data.attr('data-url');
 
   $('.js-projectname').html(projectName);
   $('.js-projecturl').html(projectURL);
@@ -21,6 +21,13 @@ $(document).on('offCanvasPanelOpen', function(event, $data) {
   $imageSets.each(function() {
     var $set = $(this);
     var selector = '.' + $set.attr('class');
+    var screenshotCount = $data.attr('data-screenshots');
+
+    var pageControl = $('.js-gallery-pagination > li:first-child').clone();
+    $('.js-gallery-pagination').html('');
+    for(var i = 0; i < screenshotCount; i++) {
+
+    }
 
     $target = $target.children(selector);
     $target.html($set.children().clone());
