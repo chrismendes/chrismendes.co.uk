d7aa7155adde6fa14e142ace8cf4ebcd11e25fc8
Chris Mendes <inbox@chrismendes.co.uk>
2015-08-28 08:20:22 +0100

    Added home page skills/expertise logo fade to colour transition

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,3 +1,19 @@
+// ---
+// In-View Animation
+// ---
+require('jquery.inview/jquery.inview');
+
+$('.js-reveal').bind('inview', function(event, isInView, visiblePartX, visiblePartY) {
+  if(isInView && visiblePartY === 'top') {
+    $('.js-reveal-element').addClass('is-revealed');
+    $(this).unbind('inview');
+  }
+});
+
+
+// ---
+// Past Projects Gallery
+// ---
 var gallery = require('gallery/gallery');
 
 var $galleryArea = $('.js-gallery');
