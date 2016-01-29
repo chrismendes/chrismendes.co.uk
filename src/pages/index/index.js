f14e87f0ff1579945de72dc1c98d1f16e02a55e5
Chris Mendes <inbox@chrismendes.co.uk>
2016-01-29 02:49:48 +0000

    Withdrawn redundant posterscreen component

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -9,23 +9,3 @@ $('.js-reveal').bind('inview', function(event, isInView, visiblePartX, visiblePa
     $(this).unbind('inview');
   }
 });
-
-
-// ---
-// Past Projects Gallery
-// ---
-var gallery = require('gallery/gallery');
-
-var $galleryArea = $('.js-gallery');
-var totalSlides = 2;
-
-gallery.initialise($galleryArea, totalSlides, {
-  container: '.js-gallery-container'
-});
-
-
-// ---
-// Past Projects Posters
-// ---
-var posterScreen = require('poster/posterscreen');
-posterScreen.initialise();
